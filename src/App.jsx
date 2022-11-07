13582ac7f5fbdf91d0fc291a113982c204236caa
Robin Lu <robinswaylu@gmail.com>
2022-11-07 18:33:44 +0800

    feat: add react router for client side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ExchangeDetail from './pages/ExchangeDetail';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exchange/:id" element={<ExchangeDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/ExchangeDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ExchangeDetail = () => {
+  return <div>ExchangeDetail</div>;
+};
+
+export default ExchangeDetail;

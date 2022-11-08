b5bd6f7bb68a1eaf0c2e7cce97b08281143745d4
Robin Lu <robinswaylu@gmail.com>
2022-11-08 17:22:53 +0800

    docs: add website screenshot for documentation

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,43 +1,27 @@
 import binance from './binance.png';
 import coinbase from './coinbase.png';
-import quotes from './quotes.svg';
-import send from './Send.svg';
-import shield from './Shield.svg';
-import star from './Star.svg';
 import menu from './menu.svg';
 import close from './close.svg';
 import google from './google.svg';
 import apple from './apple.svg';
 import arrow from './arrow-up.svg';
-import discount from './Discount.svg';
 import facebook from './facebook.svg';
 import instagram from './instagram.svg';
 import linkedin from './linkedin.svg';
 import twitter from './twitter.svg';
-import people01 from './people01.png';
-import people02 from './people02.png';
-import people03 from './people03.png';
 import logo from './logo.png';
 
 export {
   binance,
   coinbase,
   logo,
-  quotes,
-  send,
-  shield,
-  star,
   menu,
   close,
   google,
   apple,
   arrow,
-  discount,
   facebook,
   instagram,
   linkedin,
   twitter,
-  people01,
-  people02,
-  people03,
 };
